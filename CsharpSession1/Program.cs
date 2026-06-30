9477b0312f17ca8de4a854076c4d2c535e301a10
Khalid Al Hashemi <khalidalhashemi199@gmail.com>
2026-06-30 22:43:28 +0400

    Second Code: To use operators

--- CsharpSession1/Program.cs
+++ CsharpSession1/Program.cs
@@ -6,24 +6,41 @@
         {
             // Input & Output Program
 
-            Console.WriteLine("Welcome to Spark to Code!");
+            //Console.WriteLine("Welcome to Spark to Code!");
 
-            Console.WriteLine("Please enter your name: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("Hello, " + name + " Nice to meet you.");
+            //Console.WriteLine("Please enter your name: ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Hello, " + name + " Nice to meet you.");
 
-            Console.WriteLine("Please enter your age: ");
-            int age = int.Parse(Console.ReadLine());
-            Console.WriteLine("You are " + age + " years old.");
+            //Console.WriteLine("Please enter your age: ");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine("You are " + age + " years old.");
 
-            Console.WriteLine("Please enter your height: ");
-            float height = float.Parse(Console.ReadLine());
-            Console.WriteLine("Yor are " + height + " meters");
+            //Console.WriteLine("Please enter your height: ");
+            //float height = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Yor are " + height + " meters");
             ///////////////////////////////////////////////////////////////////////////////////
-         
-          
-       
-            
+
+
+
+            // Operations Program
+
+            Console.WriteLine("Enter first number: ");
+            float num1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number: ");
+            float num2 = float.Parse(Console.ReadLine());
+
+            float SumResult = num1 + num2;
+            float SubResult = num1 - num2;
+            float MulResult = num1 * num2;
+            float DivResult = num1 / num2;
+
+            Console.WriteLine("Your Sum is: " + SumResult);
+            Console.WriteLine("Your Sub is: " + SubResult);
+            Console.WriteLine("Your Mul is: " + MulResult);
+            Console.WriteLine("Your Div is: " + DivResult);
+            ///////////////////////////////////////////////////////////////////////////////////
         }
     }
 }

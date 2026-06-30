a66f17ea7864886ad0505de63952bda01c1b0daf
Khalid Al Hashemi <khalidalhashemi199@gmail.com>
2026-06-30 22:32:39 +0400

    First code: Input & Output

---
+++ CsharpSession1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CsharpSession1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input & Output Program
+
+            Console.WriteLine("Welcome to Spark to Code!");
+
+            Console.WriteLine("Please enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + " Nice to meet you.");
+
+            Console.WriteLine("Please enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("You are " + age + " years old.");
+
+            Console.WriteLine("Please enter your height: ");
+            float height = float.Parse(Console.ReadLine());
+            Console.WriteLine("Yor are " + height + " meters");
+            ///////////////////////////////////////////////////////////////////////////////////
+         
+          
+       
+            
+        }
+    }
+}

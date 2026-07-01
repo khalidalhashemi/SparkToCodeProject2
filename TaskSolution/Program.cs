3b42976ed13887b8519e89deb856158b592d444b
Khalid Al Hashemi <khalidalhashemi199@gmail.com>
2026-07-01 09:55:26 +0400

    tt

---
+++ TaskSolution/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskSolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

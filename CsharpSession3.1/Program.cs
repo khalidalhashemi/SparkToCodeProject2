7e58e36717e5ab46c72f522be6fc31015dd38d4a
Khalid Al Hashemi <khalidalhashemi199@gmail.com>
2026-07-03 18:36:59 +0400

    While Loop

--- CsharpSession3.1/Program.cs
+++ CsharpSession3.1/Program.cs
@@ -8,16 +8,40 @@
 
             // For Loop statement
 
-            int counter = 0;
+            //int counter = 0;
 
-            for(counter = 10; counter >= 1; counter--)
-            {
-                Console.WriteLine("Your count start: " + counter);
-            }
+            //for(counter = 10; counter >= 1; counter--)
+            //{
+            //    Console.WriteLine("Your count start: " + counter);
+            //}
 
-            Console.WriteLine("Counter finished! ");
+            //Console.WriteLine("Counter finished! ");
 
             ////////////////////////////////////////////////////////////////////////////////
+            ///
+
+
+            // While Loop
+
+
+            bool exitDecision = false;
+            string userInput;
+
+            while (exitDecision == false)
+            {
+                Console.WriteLine("print menu");
+
+                Console.WriteLine("Do you want to exit? (yes/no)");
+                userInput = Console.ReadLine();
+
+                if (userInput == "yes")
+                {
+                    exitDecision = true;
+                }
+            }
+
+            ////////////////////////////////////////////////////////////////////////////////
+
         }
     }
 }

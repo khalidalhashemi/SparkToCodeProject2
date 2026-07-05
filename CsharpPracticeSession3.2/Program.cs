7203ea4e5a86cfce916634a3236781f4cb84867c
Khalid Al Hashemi <khalidalhashemi199@gmail.com>
2026-07-05 17:09:42 +0400

    Add practice file

---
+++ CsharpPracticeSession3.2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CsharpPracticeSession3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

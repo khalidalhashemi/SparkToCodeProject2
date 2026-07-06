61eaaf0ed19fcccdd1024eb522699f2c40c41af5
Khalid Al Hashemi <khalidalhashemi199@gmail.com>
2026-07-06 12:44:20 +0400

    basic math operation 2

--- CsharpPracticeSession3.2/Program.cs
+++ CsharpPracticeSession3.2/Program.cs
@@ -7,17 +7,24 @@
             // Built-in function call
             // className.functionName(Parameter)
 
-            Math.Cos(90);
-            Math.Pow(2, 3);
-            Console.WriteLine("Print");
-            //Console.Clear();
+            //Math.Cos(90);
+            //Math.Pow(2, 3);
+            //Console.WriteLine("Print");
+            ////Console.Clear();
 
-            double result = Math.Cos(90);
-            Console.WriteLine(result);
+            //double result = Math.Cos(90);
+            //Console.WriteLine(result);
 
-            Console.WriteLine(Math.Pow(2, 3));
+            //Console.WriteLine(Math.Pow(2, 3));
 
             ////////////////////////////////////////////
+            ///
+
+            Console.WriteLine("Enter number: ");
+            float userInput = float.Parse(Console.ReadLine());
+
+            double result = Math.Cos(userInput);
+            Console.WriteLine(result);
         }
     }
 }
